5d163d556921d4feb4eacdc63200a4a270b14fa6
iccugs <emahaffey1@gmail.com>
2024-08-29 22:43:35 -0500

    included lambda import

--- lib/seim-cdk-stack.ts
+++ lib/seim-cdk-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class SeimCdkStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
