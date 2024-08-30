b1f0fbe9c6a43b50915793e32ab6a5d0756e9dca
iccugs <emahaffey1@gmail.com>
2024-08-29 22:40:16 -0500

    Initial commit

--- lib/seim-cdk-stack.ts
+++ lib/seim-cdk-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SeimCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        // Constructs are defined here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SeimCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // Lambda function service
+        const seimFunction = new lambda.Function(this, "SeimFunction", {
+            runtime: lambda.Runtime.PYTHON_3_12, // Execution environment
+            handler: "index.handler",
+            code: lambda.Code.fromInline(`
+                exports.handler = async function(event) {
+                    return {
+                        statusCode: 200,
+                        body: JSON.stringify("Seim Test Lambda Function"),
+                    };
+                };
+            `),
+        });
+    }
 }
