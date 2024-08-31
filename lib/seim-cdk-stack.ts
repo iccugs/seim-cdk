0604ae524946ba40894e1341fff8ceff5116f722
iccugs <emahaffey1@gmail.com>
2024-08-31 00:17:14 -0500

    added sns and sqs. modified lambda.

--- lib/seim-cdk-stack.ts
+++ lib/seim-cdk-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
 
 export class SeimCdkStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,18 +11,22 @@ export class SeimCdkStack extends cdk.Stack {
 
         // Constructs are defined here
 
+        // Simple Queue Service
+        const queue = new sqs.Queue(this, 'SeimQueue', {
+            visibilityTimeout: cdk.Duration.seconds(300)
+        });
+
+        // Simple Notification Service
+        const topic = new sns.Topic(this, 'SeimTopic');
+
+        // Add subscription to topic
+        topic.addSubscription(new subs.SqsSubscription(queue));
+
         // Lambda function service
         const seimFunction = new lambda.Function(this, "SeimFunction", {
             runtime: lambda.Runtime.NODEJS_20_X, // Execution environment
-            handler: "index.handler",
-            code: lambda.Code.fromInline(`
-                exports.handler = async function(event) {
-                    return {
-                        statusCode: 200,
-                        body: JSON.stringify('Seim Test Lambda Function'),
-                    };
-                };
-            `),
+            code: lambda.Code.fromAsset("lambda"), // Code location
+            handler: "seim.handler", // File is "seim", function is "handler"
         });
 
         // Lambda function URL resource
